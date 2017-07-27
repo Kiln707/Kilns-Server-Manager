d311bbdac59f04823c700d286ab7c5a1b4ee2efb
kiln707 <steven.guy.swanson@gmail.com>
2017-07-27 16:13:22 -0700

    Created Socket server for console and network use.

--- server.py
+++ server.py
@@ -1,22 +1,32 @@
 #! /usr/bin/python3
 
-import os, subprocess, sys, datetime, time
+import os, subprocess, sys, datetime, time, socket
 import rethinkdb as r
+
+############################################
+# Log class
+# Handles writing to console and log files
+############################################
 
 class Log:
     def __init__(self, location, filename):
+        global print
         if os.path.isfile(os.path.join(location, filename)):
             os.remove(os.path.join(location, filename))
         self.logfile=open(os.path.join(location, filename), "w")
         self.console=sys.stdout
         self.error=sys.stderr
-        self.console.write("Initialized Log")
+        print = self.log #Make it so that whenever we use print, it will call logger.log.
+        print("Initialized Log.")
 
     def close(self):
-        close(logfile)
+        self.log("Closing Logs.")
+        self.logfile.close()
+        self.console.close()
+        self.error.close()
 
     def log(self,message,level='info'):
-        output='['+str(datetime.datetime.now())+'] ['+level.upper()+'] '+str(message)
+        output='['+str(datetime.datetime.now())+'] ['+level.upper()+'] '+str(message)+"\n\r"
         self.logfile.write(output)
         if level == "error":
             self.error.write(output)
@@ -32,6 +42,9 @@ def __charPos(char, string, pos=0):
                         return i
         return -1
 
+################################
+#   Configuration file reading
+################################
 def getConfigs(location):
     cfg = { }
     if os.path.isfile(location): #If the configuration file exists
@@ -50,27 +63,66 @@ def getConfigs(location):
                     value = value[0:len(value)-1] #End removing Unnecessary Quotes
                 cfg[key] = value.replace('\n', '').replace('\t','').replace('\r','') #Set the value of the
                 line = cfgFile.readline()
-    print(cfg)
     return cfg
 
-def startDatabase(cfg, cwd):
-    print("Starting database")
+###################################
+# Networking/Console Section
+###################################
+def initializeNetworking(cfg):
+    global logger
+    print("Initilizing networking...")
+    consoleSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    networkSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        consoleSocket.bind( ('localhost', 8889) )
+        networkSocket.bind( (cfg['bind_ip'], int(cfg['bind_port'])) )
+    except socket.error as msg:
+        logger.log("Failed to bind to",cfg['bind_ip']+":"+cfg['bind_port'],"Error Code:", str(msg[0]), 'Message:',msg[1])
+    consoleSocket.listen(5)
+    networkSocket.listen(5)
+    print("Network intitialization complete.")
+    return networkSocket, consoleSocket
+
+def clientConnectionHandler():
+    pass
+
+def shutdownNetworking(consoleSocket, networkSocket):
+    print("Shutting down networking...")
+    consoleSocket.close()
+    networkSocket.close()
+    print('Network shutdown complete.')
+
+###################################
+# Database Section
+##################################
+def initializeDatabase(cfg, cwd):
+    print("Initilizing database...")
     if 'use_database' not in cfg:
-        return -1
+        sys.exit(-1) #Error, should not happpen
     if cfg['use_database'] == "False":
         db = subprocess.Popen(os.path.join(cwd,"rethinkdb.exe"), stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         conn = None
         while conn is None:
             conn = r.connect('localhost', 28015)
+        print("Database intitialization complete.")
         return db,conn
     else:
         pass
         #Load connection to database.
 
-def stopDatabase(databaseObj, connection):
+def queryDatabase():
+    pass
+
+def shutdownDatabase(databaseObj, connection):
+    print("Shutting down database...")
     connection.close()
     if isinstance(databaseObj, subprocess.Popen):
         databaseObj.terminate()
+    print('Database shutdown complete.')
+
+######################################
+#   Program Section
+#####################################
 
 def main():
 
@@ -83,23 +135,27 @@ def main():
 
     #Initialize the logger.
     logger = Log( os.path.join(cwd, 'logs') , filename+".log") #Create the log object
-    print = logger.log #Make it so that whenever we use print, it will call logger.log.
 
     #Load the configuration file
     cfg = getConfigs(os.path.join(cwd,filename+".cfg"))
 
-    #Initialize the socket server
-
-
     #Start the database AND/OR the DB connection
-    databaseObj, connection = startDatabase(cfg, cwd)
+    databaseObj, connection = initializeDatabase(cfg, cwd)
+
+    #Initialize the socket server
+    consoleSocket, networkSocket = initializeNetworking(cfg)
+    #######################################
+    #   SERVER RUNNING !
+    ######################################
+    while False:
+        pass #SERVER LOGIC
 
     #######################################
     #   SHUTDOWN SERVER !
     #######################################
-    stopDatabase(databaseObj, connection)
-    #stopSocketServer
-    #stopLogFile
+    shutdownNetworking(consoleSocket, networkSocket) #Closing networking
+    shutdownDatabase(databaseObj, connection) #Closing database
+    logger.close() #Closing Logger
 
 
 

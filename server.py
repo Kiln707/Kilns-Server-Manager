90a69b754acef60b04ec66f79fd532d931a11ca5
kiln707 <steven.guy.swanson@gmail.com>
2017-07-26 20:49:25 -0700

    Logger and local database created
    Created logger which captures stdout and stderr so that output is written to a file in addition to being written to console.
    Next, Create socket server to handle console connections.

--- server.py
+++ server.py
@@ -1,6 +1,30 @@
 #! /usr/bin/python3
 
-import os, subprocess
+import os, subprocess, sys, datetime, time
+import rethinkdb as r
+
+class Log:
+    def __init__(self, location, filename):
+        if os.path.isfile(os.path.join(location, filename)):
+            os.remove(os.path.join(location, filename))
+        self.logfile=open(os.path.join(location, filename), "w")
+        self.console=sys.stdout
+        self.error=sys.stderr
+        self.console.write("Initialized Log")
+
+    def close(self):
+        close(logfile)
+
+    def log(self,message,level='info'):
+        output='['+str(datetime.datetime.now())+'] ['+level.upper()+'] '+str(message)
+        self.logfile.write(output)
+        if level == "error":
+            self.error.write(output)
+        else:
+            self.console.write(output)
+
+
+
 
 def __charPos(char, string, pos=0):
         for i in range(pos+1, len(string)):
@@ -26,18 +50,56 @@ def getConfigs(location):
                     value = value[0:len(value)-1] #End removing Unnecessary Quotes
                 cfg[key] = value.replace('\n', '').replace('\t','').replace('\r','') #Set the value of the
                 line = cfgFile.readline()
+    print(cfg)
     return cfg
 
-def startDatabase(cfg):
+def startDatabase(cfg, cwd):
+    print("Starting database")
     if 'use_database' not in cfg:
         return -1
     if cfg['use_database'] == "False":
-        print('start private database')
+        db = subprocess.Popen(os.path.join(cwd,"rethinkdb.exe"), stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        conn = None
+        while conn is None:
+            conn = r.connect('localhost', 28015)
+        return db,conn
+    else:
+        pass
+        #Load connection to database.
 
+def stopDatabase(databaseObj, connection):
+    connection.close()
+    if isinstance(databaseObj, subprocess.Popen):
+        databaseObj.terminate()
 
 def main():
-    cfg = getConfigs(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__),"Service_Manager.cfg")))
-    startDatabase(cfg)
+
+    ##############################
+    # INITIALIZE SERVER !
+    ###############################
+    #Set variables used throughout server.
+    filename='Service_Manager'
+    cwd=os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+    #Initialize the logger.
+    logger = Log( os.path.join(cwd, 'logs') , filename+".log") #Create the log object
+    print = logger.log #Make it so that whenever we use print, it will call logger.log.
+
+    #Load the configuration file
+    cfg = getConfigs(os.path.join(cwd,filename+".cfg"))
+
+    #Initialize the socket server
+
+
+    #Start the database AND/OR the DB connection
+    databaseObj, connection = startDatabase(cfg, cwd)
+
+    #######################################
+    #   SHUTDOWN SERVER !
+    #######################################
+    stopDatabase(databaseObj, connection)
+    #stopSocketServer
+    #stopLogFile
 
 
 
